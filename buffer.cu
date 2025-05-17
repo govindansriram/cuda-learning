584570d3019fc84eefe46d00b323eed4cb3c9f0f
govindansriram <sriramgovindanwork@gmail.com>
2025-05-16 22:46:38 -0700

    added double buffer throuhg ILP

--- buffer.cu
+++ buffer.cu
@@ -13,6 +13,53 @@ __device__ __host__ __forceinline__ size_t ceil_div(const size_t top, const size
     return (top + (bottom - 1)) / bottom;
 }
 
+__device__ __host__ __forceinline__ constexpr bool is_power_of_two(const size_t x) {
+    /**
+     * first checks if the number is not 0 we do this premptive check since 0 is not a power of 2
+     * yet would pass the next check
+     *
+     * (x & (x - 1)) == 0 this checks if the number is a power of 2
+     *
+     * all powers of 2 have exactly one 1 bit e.g.:
+     * 1 0 0 0 = 1
+     * 0 1 0 0 = 2
+     * 0 0 1 0 = 4
+     * 0 0 0 1 = 8
+     *
+     * the number before of 2 has to have all 1 bits excpet at the last position
+     *
+     * 0 0 0 0 = 0
+     * 1 0 0 0 = 1
+     * 1 1 0 0 = 3
+     * 1 1 1 0 = 7
+     *
+     * so we know a number is a multiple of 2 if the bit wise and of the previous current number == 0
+     */
+    return x != 0 && (x & (x - 1)) == 0;
+}
+
+__device__ __host__ __forceinline__ constexpr size_t next_power_of_two(size_t x) {
+    /**
+     * We know that 1 - (a power of 2) is all 1 bits so to find the next power of 2
+     * our goal is to fill all bits before the highest set bit to 1 and then add
+     * 1 to it giving us our next power of 2.
+     *
+     * We also start off by subtracting 1 to handle the case where the number
+     * is already a power of 2
+     **/
+
+
+    if (x == 0) return 1;
+    --x;
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+    x |= x >> 32;
+    return x + 1;
+}
+
 template<
     size_t TILE_SIZE_X,
     size_t TILE_SIZE_Y,
@@ -63,6 +110,62 @@ __device__ __forceinline__ void load_to_shared(
             value = mat_B[B_row * B_stride + B_column];
 
         B_buffer[B_buffer_row][B_buffer_column] = value;
+    }
+}
+
+
+template<
+    size_t TILE_SIZE_X,
+    size_t TILE_SIZE_Y,
+    size_t TILE_SIZE_K,
+    size_t A_BUFFER_SIZE,
+    size_t B_BUFFER_SIZE,
+    size_t THREADS_PER_BLOCK
+>
+__device__ __forceinline__ void load_to_shared_double_buffer(
+    const size_t iter,
+    float A_buffer[A_BUFFER_SIZE],
+    float B_buffer[B_BUFFER_SIZE],
+    const float *mat_A,
+    const float *mat_B,
+    const size_t M,
+    const size_t N,
+    const size_t K,
+    const size_t thread_linear_idx,
+    const size_t A_stride,
+    const size_t B_stride) {
+    constexpr size_t A_iterations{TILE_SIZE_Y * TILE_SIZE_K / THREADS_PER_BLOCK};
+
+    for (size_t A_iter{0}; A_iter < A_iterations; ++A_iter) {
+        size_t A_buffer_row{(thread_linear_idx + A_iter * THREADS_PER_BLOCK) / TILE_SIZE_K};
+        size_t A_buffer_column{(thread_linear_idx + A_iter * THREADS_PER_BLOCK) % TILE_SIZE_K};
+
+        float value{0};
+
+        const size_t A_row{blockIdx.y * TILE_SIZE_Y + A_buffer_row};
+        const size_t A_column{A_buffer_column + iter * TILE_SIZE_K};
+
+        if (A_row < M && A_column < K)
+            value = mat_A[A_row * A_stride + A_column];
+
+        A_buffer[A_buffer_row * TILE_SIZE_K + A_buffer_column] = value;
+    }
+
+    constexpr size_t B_iterations{TILE_SIZE_K * TILE_SIZE_X / THREADS_PER_BLOCK};
+
+    for (size_t B_iter{0}; B_iter < B_iterations; ++B_iter) {
+        size_t B_buffer_row{(thread_linear_idx + B_iter * THREADS_PER_BLOCK) / TILE_SIZE_X};
+        size_t B_buffer_column{(thread_linear_idx + B_iter * THREADS_PER_BLOCK) % TILE_SIZE_X};
+
+        float value{0};
+
+        const size_t B_row{B_buffer_row + iter * TILE_SIZE_K};
+        const size_t B_column{blockIdx.x * TILE_SIZE_X + B_buffer_column};
+
+        if (B_row < K && B_column < N)
+            value = mat_B[B_row * B_stride + B_column];
+
+        B_buffer[B_buffer_row * TILE_SIZE_X + B_buffer_column] = value;
     }
 }
 
@@ -155,6 +258,127 @@ __device__ __forceinline__ void producer(
     }
 }
 
+template<size_t TILE_SIZE_X, size_t TILE_SIZE_K>
+__device__ __forceinline__ void accumulate(
+    float &partial,
+    const uintptr_t A_block_load_addr,
+    const uintptr_t B_block_load_addr) {
+
+    const auto A_load_ptr{reinterpret_cast<float *>(A_block_load_addr)};
+    const auto B_load_ptr{reinterpret_cast<float *>(B_block_load_addr)};
+
+    for (size_t k{0}; k < TILE_SIZE_K; ++k)
+        partial += A_load_ptr[threadIdx.y * TILE_SIZE_X + k] * B_load_ptr[k * TILE_SIZE_X + threadIdx.x];
+
+}
+
+template<
+    size_t THREADS_PER_BLOCK,
+    size_t TILE_SIZE_X,
+    size_t TILE_SIZE_Y,
+    size_t TILE_SIZE_K>
+__global__ void gemm_double_buffering(
+    float *mat_A,
+    float *mat_B,
+    float *mat_C,
+    const size_t M,
+    const size_t N,
+    const size_t K,
+    const size_t A_stride,
+    const size_t B_stride,
+    const size_t C_stride) {
+    // implementation derived from https://salykova.github.io/sgemm-gpu section 5.1
+
+    constexpr size_t A_buffer_size{next_power_of_two(2 * TILE_SIZE_Y * TILE_SIZE_K * sizeof(float))};
+    constexpr size_t B_buffer_size{next_power_of_two(2 * TILE_SIZE_K * TILE_SIZE_X * sizeof(float))};
+
+    __shared__ float __align__(A_buffer_size) A_buffer[A_buffer_size / sizeof(float)];
+    __shared__ float __align__(B_buffer_size) B_buffer[B_buffer_size / sizeof(float)];
+
+    const size_t iterations{ceil_div(K, TILE_SIZE_K) - 1};
+
+    const size_t thread_linear_idx{threadIdx.y * TILE_SIZE_X + threadIdx.x};
+    const size_t C_row{blockIdx.y * TILE_SIZE_Y + threadIdx.y};
+    const size_t C_column{blockIdx.x * TILE_SIZE_X + threadIdx.x};
+
+    load_to_shared_double_buffer<
+        TILE_SIZE_X,
+        TILE_SIZE_Y,
+        TILE_SIZE_K,
+        A_buffer_size,
+        B_buffer_size,
+        THREADS_PER_BLOCK>(
+        0,
+        A_buffer,
+        B_buffer,
+        mat_A,
+        mat_B,
+        M,
+        N,
+        K,
+        thread_linear_idx,
+        A_stride,
+        B_stride
+    );
+
+    __syncthreads();
+
+    auto A_block_store_addr = reinterpret_cast<uintptr_t>(A_buffer);
+    auto A_block_load_addr = reinterpret_cast<uintptr_t>(A_buffer);
+
+    auto B_block_store_addr = reinterpret_cast<uintptr_t>(B_buffer);
+    auto B_block_load_addr = reinterpret_cast<uintptr_t>(B_buffer);
+
+    constexpr uintptr_t flip_bits_A{A_buffer_size / 2};
+    constexpr uintptr_t flip_bits_B{A_buffer_size / 2};
+
+    A_block_store_addr ^= flip_bits_A;
+    B_block_store_addr ^= flip_bits_B;
+
+    float partial{0};
+
+    for (size_t iter{0}; iter < iterations; ++iter) {
+        auto A_store_ptr{reinterpret_cast<float *>(A_block_store_addr)};
+        auto B_store_ptr{reinterpret_cast<float *>(B_block_store_addr)};
+
+        load_to_shared_double_buffer<
+            TILE_SIZE_X,
+            TILE_SIZE_Y,
+            TILE_SIZE_K,
+            A_buffer_size,
+            B_buffer_size,
+            THREADS_PER_BLOCK>(
+            0,
+            A_store_ptr,
+            B_store_ptr,
+            mat_A,
+            mat_B,
+            M,
+            N,
+            K,
+            thread_linear_idx,
+            A_stride,
+            B_stride
+        );
+
+        accumulate<TILE_SIZE_X, TILE_SIZE_K>(partial, A_block_load_addr, B_block_load_addr);
+
+        A_block_load_addr ^= flip_bits_A;
+        A_block_store_addr ^= flip_bits_A;
+
+        B_block_load_addr ^= flip_bits_B;
+        B_block_store_addr ^= flip_bits_B;
+
+        __syncthreads();
+    }
+
+    accumulate<TILE_SIZE_X, TILE_SIZE_K>(partial, A_block_load_addr, B_block_load_addr);
+
+    if (C_row < M && C_column < N) {
+        mat_C[C_row * C_stride + C_column] = partial;
+    }
+}
+
 
 template<
     size_t CONSUMER_WARPS,
@@ -173,7 +397,6 @@ __global__ void producer_consumer_pattern(
     const size_t A_stride,
     const size_t B_stride,
     const size_t C_stride) {
-
     // CONSUMER_WARPS will handle processing the data
     // PRODUCER_WARPS will handle writing the data to memory
 
@@ -319,12 +542,11 @@ void cpu_matmul_naive(
 }
 
 void test_equivalency(
-    const float * expected,
-    const float * result,
+    const float *expected,
+    const float *result,
     const size_t M,
     const size_t N,
     const size_t stride) {
-
     for (size_t i{0}; i < M; ++i) {
         for (size_t j{0}; j < N; ++j) {
             assert(expected[i * stride + j] == result[i * stride + j]);
@@ -347,13 +569,12 @@ int generate_random_int(const int min_val, const int max_val) {
 }
 
 void fill_matrix(
-    float * matrix,
+    float *matrix,
     const size_t M,
     const size_t N,
     const size_t stride,
     const float min_val,
     const float max_val) {
-
     for (size_t i{0}; i < M; ++i) {
         for (size_t j{0}; j < N; ++j) {
             matrix[i * stride + j] = generate_random_float(min_val, max_val);
@@ -362,13 +583,12 @@ void fill_matrix(
 }
 
 void fill_matrix_w(
-    float * matrix,
+    float *matrix,
     const size_t M,
     const size_t N,
     const size_t stride,
     const int min_val,
     const int max_val) {
-
     for (size_t i{0}; i < M; ++i) {
         for (size_t j{0}; j < N; ++j) {
             matrix[i * stride + j] = static_cast<float>(generate_random_int(min_val, max_val));
@@ -378,11 +598,10 @@ void fill_matrix_w(
 
 
 void print_matrix(
-    const float * matrix,
+    const float *matrix,
     const size_t M,
     const size_t N,
     const size_t stride) {
-
     for (size_t i{0}; i < M; ++i) {
         std::cout << "[";
         for (size_t j{0}; j < N; ++j) {
@@ -402,9 +621,9 @@ void test_regular_shared_mem() {
     auto host_C{new float[211 * 68]};
     auto host_C_2{new float[211 * 68]};
 
-    float * dev_A;
-    float * dev_B;
-    float * dev_C;
+    float *dev_A;
+    float *dev_B;
+    float *dev_C;
 
     fill_matrix_w(host_A, 211, 35, 35, -100, 100);
     fill_matrix_w(host_B, 35, 68, 68, -100, 100);
@@ -445,16 +664,16 @@ void test_regular_shared_mem() {
         TILE_SIZE_X,
         TILE_SIZE_Y,
         TILE_SIZE_K><<<grid_dim, block_dim>>>(
-            dev_A,
-            dev_B,
-            dev_C,
-            211,
-            68,
-            35,
-            35,
-            68,
-            68
-        );
+        dev_A,
+        dev_B,
+        dev_C,
+        211,
+        68,
+        35,
+        35,
+        68,
+        68
+    );
 
     cudaDeviceSynchronize();
 
@@ -486,9 +705,9 @@ void test_double_buffer() {
     auto host_C{new float[211 * 68]};
     auto host_C_2{new float[211 * 68]};
 
-    float * dev_A;
-    float * dev_B;
-    float * dev_C;
+    float *dev_A;
+    float *dev_B;
+    float *dev_C;
 
     fill_matrix_w(host_A, 211, 35, 35, -100, 100);
     fill_matrix_w(host_B, 35, 68, 68, -100, 100);
@@ -534,16 +753,16 @@ void test_double_buffer() {
         TILE_SIZE_X,
         TILE_SIZE_Y,
         TILE_SIZE_K><<<grid_dim, block_dim>>>(
-            dev_A,
-            dev_B,
-            dev_C,
-            211,
-            68,
-            35,
-            35,
-            68,
-            68
-        );
+        dev_A,
+        dev_B,
+        dev_C,
+        211,
+        68,
+        35,
+        35,
+        68,
+        68
+    );
 
     cudaDeviceSynchronize();
 
@@ -569,9 +788,9 @@ void time_shared_memory() {
     auto host_A{new float[4096 * 4096]};
     auto host_B{new float[4096 * 4096]};
 
-    float * dev_A;
-    float * dev_B;
-    float * dev_C;
+    float *dev_A;
+    float *dev_B;
+    float *dev_C;
 
     fill_matrix(host_A, 4096, 4096, 4096, -100.f, 100.f);
     fill_matrix(host_B, 4096, 4096, 4096, -100.f, 100.f);
@@ -604,16 +823,16 @@ void time_shared_memory() {
         TILE_SIZE_X,
         TILE_SIZE_Y,
         TILE_SIZE_K><<<grid_dim, block_dim>>>(
-            dev_A,
-            dev_B,
-            dev_C,
-            4096,
-            4096,
-            4096,
-            4096,
-            4096,
-            4096
-        );
+        dev_A,
+        dev_B,
+        dev_C,
+        4096,
+        4096,
+        4096,
+        4096,
+        4096,
+        4096
+    );
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
 
@@ -637,9 +856,9 @@ void time_double_buffer() {
     auto host_A{new float[4096 * 4096]};
     auto host_B{new float[4096 * 4096]};
 
-    float * dev_A;
-    float * dev_B;
-    float * dev_C;
+    float *dev_A;
+    float *dev_B;
+    float *dev_C;
 
     fill_matrix(host_A, 4096, 4096, 4096, -100.f, 100.f);
     fill_matrix(host_B, 4096, 4096, 4096, -100.f, 100.f);
@@ -677,16 +896,16 @@ void time_double_buffer() {
         TILE_SIZE_X,
         TILE_SIZE_Y,
         TILE_SIZE_K><<<grid_dim, block_dim>>>(
-            dev_A,
-            dev_B,
-            dev_C,
-            4096,
-            4096,
-            4096,
-            4096,
-            4096,
-            4096
-        );
+        dev_A,
+        dev_B,
+        dev_C,
+        4096,
+        4096,
+        4096,
+        4096,
+        4096,
+        4096
+    );
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
 
