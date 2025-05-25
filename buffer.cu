a37888bf5ee4e0af48d957b9b95585794994f061
govindansriram <sriramgovindanwork@gmail.com>
2025-05-25 12:12:53 -0700

    added base testing

--- buffer.cu
+++ buffer.cu
@@ -223,7 +223,7 @@ template<
     size_t COPY_ITERATIONS,
     size_t BLOCK_TILE_SKEW_K = 0
 >
-__device__ __forceinline__ void load_data_to_shared_matrix_A(
+__device__ __forceinline__ void load_data_to_shared_matrix_A_async(
     T A_shared[BLOCK_TILE_SIZE_Y][BLOCK_TILE_SIZE_K + BLOCK_TILE_SKEW_K],
     const T *A_matrix,
     const size_t m,
@@ -416,15 +416,15 @@ __device__ void load_data_to_shared_async(
             A_shared_pipeline
         );
     } else {
-        load_data_to_shared_matrix_A<
+        load_data_to_shared_matrix_A_async<
             T, BLOCK_TILE_SIZE_Y, BLOCK_TILE_SIZE_K,
             THREADS_PER_BLOCK, COPY_ITERATIONS_A,
             BLOCK_TILE_SKEW_Y
         >(
             A_shared,
             A_matrix,
-            k,
             m,
+            k,
             leading_dimension_A,
             iteration,
             thread_linear_idx,
@@ -1556,6 +1556,7 @@ __global__ void gemm_2DBT_async(
             B_priors,
             0,
             0,
+            0,
             false
         >(
             A_buffer[stage],
@@ -1606,6 +1607,7 @@ __global__ void gemm_2DBT_async(
             total_threads,
             A_priors,
             B_priors,
+            0,
             0,
             0,
             false
@@ -2369,7 +2371,7 @@ void flush_l2_cache() {
         std::cerr << "CUDA error: " << cudaGetErrorString(error) << std::endl;
 }
 
-void time_shared_memory() {
+void time_2DBT() {
     auto host_A{new float[4096 * 4096]};
     auto host_B{new float[4096 * 4096]};
 
@@ -2409,6 +2411,82 @@ void time_shared_memory() {
         float milliseconds = 0;
         cudaEventRecord(start);
         gemm_2DBT<
+            TILE_SIZE_X,
+            TILE_SIZE_Y,
+            TILE_SIZE_K><<<grid_dim, block_dim>>>(
+            dev_A,
+            dev_B,
+            dev_C,
+            4096,
+            4096,
+            4096,
+            4096,
+            4096,
+            4096
+        );
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&milliseconds, start, stop);
+
+        if (i > 185) total += milliseconds;
+        flush_l2_cache();
+    }
+
+    std::cout << total / 185 << "\n";
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    cudaFree(dev_A);
+    cudaFree(dev_B);
+    cudaFree(dev_C);
+
+    delete []host_A;
+    delete []host_B;
+
+    flush_l2_cache();
+}
+
+void time_gemm_2DBT_async() {
+    auto host_A{new float[4096 * 4096]};
+    auto host_B{new float[4096 * 4096]};
+
+    float *dev_A;
+    float *dev_B;
+    float *dev_C;
+
+    fill_matrix(host_A, 4096, 4096, 4096, -100.f, 100.f);
+    fill_matrix(host_B, 4096, 4096, 4096, -100.f, 100.f);
+
+    cudaMalloc(&dev_A, 4096 * 4096 * sizeof(float));
+    cudaMalloc(&dev_B, 4096 * 4096 * sizeof(float));
+    cudaMalloc(&dev_C, 4096 * 4096 * sizeof(float));
+
+    cudaMemcpy(dev_A, host_A, 4096 * 4096 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_B, host_B, 4096 * 4096 * sizeof(float), cudaMemcpyHostToDevice);
+
+    constexpr size_t TILE_SIZE_X{16};
+    constexpr size_t TILE_SIZE_Y{16};
+    constexpr size_t TILE_SIZE_K{16};
+
+    constexpr size_t THREADS_PER_BLOCK{TILE_SIZE_X * TILE_SIZE_Y};
+
+    static_assert(TILE_SIZE_K * TILE_SIZE_X % THREADS_PER_BLOCK == 0);
+    static_assert(TILE_SIZE_K * TILE_SIZE_Y % THREADS_PER_BLOCK == 0);
+
+    constexpr dim3 block_dim(TILE_SIZE_X, TILE_SIZE_Y);
+    const dim3 grid_dim(ceil_div(4096, TILE_SIZE_X), ceil_div(4096, TILE_SIZE_Y));
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    float total{0};
+
+    for (int i{0}; i < 371; ++i) {
+        float milliseconds = 0;
+        cudaEventRecord(start);
+        gemm_2DBT_async<
             TILE_SIZE_X,
             TILE_SIZE_Y,
             TILE_SIZE_K><<<grid_dim, block_dim>>>(
@@ -2816,11 +2894,12 @@ void run_double_buffer_test() {
     // test_regular_shared_mem();
     // test_double_buffer_gemm();
     // time_db_gemm_memory();
-    test_gemm_2DBT_async();
+    // test_gemm_2DBT_async();
     // test_async_load_gemm();
-    // time_shared_memory();
-    // time_gemm_2DBT_2DWT_2DTT_vload();
-    // time_gemm_2DBT_2DWT_2DTT_async();
+    // time_2DBT();
+    // time_gemm_2DBT_async();
+    time_gemm_2DBT_2DWT_2DTT_vload();
+    time_gemm_2DBT_2DWT_2DTT_async();
     // test_gemm_2DBT_2DWT_2DTT_vload();
     // time_double_buffer();
 }
